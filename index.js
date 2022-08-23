536b840ec2550f63a03164776dd8a1815a8cec22
Quinn <quinn@indra.ai>
2022-08-23 15:46:59 -0700

    moving to feecting scope

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const data_path = path.join(__dirname, 'data.json');
 const {agent,vars} = require(data_path).data;
 
-const Deva = require('@indra.ai/deva');
+const Deva = require('@feecting/deva');
 const VEDAS = new Deva({
   agent: {
     uid: agent.uid,
